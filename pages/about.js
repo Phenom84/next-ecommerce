e97516c72105baa9af8f455aaf7c0c642f963913
Cerneaga Denis <prolevelmd@gmail.com>
2020-05-12 02:59:11 +0300

    Create project next-ecommerce

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Navigation from '../components/Navigation'
+
+export default () => {
+    return (
+        <div>
+            <Navigation />
+           <h1>About</h1> 
+        </div>
+    );
+};
---
+++ pages/pravicy_and_policy.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Navigation from '../components/Navigation'
+
+export default () => {
+    return (
+        <div>
+            <Navigation />
+           <h1>Privacy and Policy</h1> 
+        </div>
+    );
+};
