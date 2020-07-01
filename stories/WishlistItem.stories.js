443cff67d29c9f600fee580ab087547be64294ae
Cerneaga Denis <cerneaga.denis@gmail.com>
2020-07-01 18:02:13 +0300

    Fix WishlistItem.stories.js

--- stories/WishlistItem.stories.js
+++ stories/WishlistItem.stories.js
@@ -1,9 +1,28 @@
 import React from "react";
 import WishListItem from "./../components/WishListItem";
-import { GlobalProvider } from "../src/context/GlobalContext";
-import { IntlProvider } from "react-intl";
-import translations from "./../components/translations";
 import { withNextRouter } from "./../.storybook/decorators/NextRouterDecorator";
+
+const product = {
+  id: "1",
+  category: "IQ Puzzle",
+  name: "Rachtangle",
+  price: "$ 3.00",
+  description:
+    'The figure has 4 corners, in the puzzle there are 4 parts. "Everything is fine, boss, I can handle it quickly," the brain reports. Then the fun begins! The brain discovers that the figures have 3 and 5 sides. They also vary greatly in size! How to put them in a simple rectangle? You will know this if you try to assemble the Rectangle.',
+  image:
+    "https://raw.githubusercontent.com/Phenom84/next-ecommerce/master/public/p1.webp",
+  rating: 3.8,
+  images: [
+    {
+      src: "/p1.webp",
+      alt: "Front view",
+    },
+    {
+      src: "/back1.webp",
+      alt: "Back view",
+    },
+  ],
+};
 
 export default {
   title: "WishListItem",
@@ -11,10 +30,4 @@ export default {
   decorators: [withNextRouter],
 };
 
-export const Default = () => (
-  <GlobalProvider>
-    <IntlProvider locale={"en"} messages={translations["en"]}>
-      <WishListItem />
-    </IntlProvider>
-  </GlobalProvider>
-);
+export const Default = () => <WishListItem product={product} />;
