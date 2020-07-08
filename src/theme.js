bfef7130da9f25bb7bf6295367aa10b580611466
Cerneaga Denis <cerneaga.denis@gmail.com>
2020-07-09 00:01:12 +0300

    Change start url in manifest url second try

--- src/theme.js
+++ src/theme.js
@@ -1,67 +1,67 @@
-import { createMuiTheme } from "@material-ui/core/styles";
+import { createMuiTheme } from '@material-ui/core/styles';
 
 const theme = createMuiTheme({
   palette: {
-    type: "dark",
+    type: 'dark',
     secondary: {
-      main: "rgba(247, 149, 72, 1)",
+      main: 'rgba(247, 149, 72, 1)',
     },
     background: {
-      paper: "rgba(255, 255, 255, 0.08)",
+      paper: 'rgba(255, 255, 255, 0.08)',
     },
   },
   props: {
     MuiLink: {
-      color: "inherit",
-      underline: "none",
+      color: 'inherit',
+      underline: 'none',
     },
   },
   overrides: {
     MuiButton: {
       outlined: {
-        borderColor: "rgba(247, 149, 72, 1)",
+        borderColor: 'rgba(247, 149, 72, 1)',
       },
     },
     MuiTypography: {
       h4: {
-        lineHeight: "2em",
+        lineHeight: '2em',
       },
     },
     MuiContainer: {
       root: {
-        padding: "1em 0 1em 0",
+        padding: '1em 0 1em 0',
       },
     },
     MuiAppBar: {
       root: {
-        backgroundImage: "url(back.webp)",
-        backgroundSize: "cover",
-        backgroundPosition: "top center",
+        backgroundImage: 'url(back.webp)',
+        backgroundSize: 'cover',
+        backgroundPosition: 'top center',
       },
     },
     MuiCssBaseline: {
-      "@global": {
+      '@global': {
         body: {
-          backgroundImage: "url(back.webp)",
-          backgroundSize: "cover",
-          backgroundPosition: "top center",
+          backgroundImage: 'url(back.webp)',
+          backgroundSize: 'cover',
+          backgroundPosition: 'top center',
         },
       },
     },
     MuiDivider: {
       root: {
-        backgroundColor: "#FFF",
+        backgroundColor: '#FFF',
       },
     },
     MuiToolbar: {
       root: {
-        height: "66px",
+        height: '66px',
       },
     },
     MuiMenu: {
       paper: {
-        backgroundColor: "rgba(255, 255, 255, 1)",
-        color: "rgba(0, 0, 0, 1)",
+        backgroundColor: 'rgba(255, 255, 255, 1)',
+        color: 'rgba(0, 0, 0, 1)',
       },
     },
   },
