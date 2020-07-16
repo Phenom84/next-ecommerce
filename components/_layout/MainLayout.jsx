eb713114ec362a181f4342817bd8e1464b4ab99b
Cerneaga Denis <cerneaga.denis@gmail.com>
2020-07-16 22:44:58 +0300

    Add next/pwa

--- components/_layout/MainLayout.jsx
+++ components/_layout/MainLayout.jsx
@@ -1,23 +1,17 @@
 import React from 'react';
 import Head from 'next/head';
-import theme from './../../src/theme';
 import { PropTypes } from 'prop-types';
 
 const MainLayout = ({ children, title = 'IQ Puzzle' }) => {
   return (
     <>
       <Head>
-        <title>{title}</title>
-        <link rel="icon" type="image/png" href="/favicon.png" />
-        <link rel="apple-touch-icon" href="/apple_touch_icon.png" />
-        <link
-          rel="manifest"
-          href="https://raw.githubusercontent.com/Phenom84/next-ecommerce/master/manifest.webmanifest"
-        />
-        <meta name="theme-color" content={theme.palette.primary.main} />
+        <meta charset="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="theme-color" content="#303030" />
         <meta
           name="viewport"
-          content="minimum-scale=1, initial-scale=1, width=device-width"
+          content="minimum-scale=1, initial-scale=1, width=device-width, maximum-scale=5"
         />
         <meta
           name="description"
@@ -28,6 +22,10 @@ const MainLayout = ({ children, title = 'IQ Puzzle' }) => {
         <meta name="keywords" content="puzzles, brain fitness, iqpuzzle" />
         <meta name="author" content="Denis Cerneaga" />
         <meta name="robots" content="index" />
+        <title>{title}</title>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="icon" type="image/png" href="/favicon.png" />
+        <link rel="apple-touch-icon" href="/apple_touch_icon.png" />
       </Head>
       <main>{children}</main>
     </>
